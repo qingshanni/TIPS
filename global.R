21943ada086dbce673911c05cb825e50a8e77c0f
qingshanni <qingshanni@foxmail.com>
2020-12-17 23:51:58 +0800

    revised

--- global.R
+++ global.R
@@ -1,20 +1,12 @@
 r_encoding <- "UTF-8"
-
 r_path <- '.'
-## pkgs used
-#r_pkgs <- c('ggplot2',"shiny", "pheatmap", "shinydashboard","NormqPCR",'limma','reshape')
-#sapply(r_pkgs, require, character.only = TRUE)
-#options(repos = BiocInstaller::biocinstallRepos())
+
 library('ggplot2')
 library("shiny")
-#library("pheatmap")
 library("shinydashboard")
 library("Seurat")
 library('monocle')
 library('kohonen')
-library("viridis") #may as use viridis to make the gene expression heatmap prettier honestly
-#library('scales')
-#library('matrixStats')
-#library('qusage')
+library("viridis") 
 library('switchde')
 options(shiny.maxRequestSize = 100*1024^2) 
